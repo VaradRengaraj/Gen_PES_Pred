0373ba684d74d67e4124a8d28c2f9e5c5da99234
Varadarajan <varadarajanrengaraj@outlook.com>
2022-01-05 17:54:39 +0100

    revert to original change, user id and email id reflects correctly

--- dataloader/custdataset.py
+++ dataloader/custdataset.py
@@ -12,7 +12,7 @@ class CustomDataset(Data.Dataset):
     phase. During loading, caching can be done for large datasets. If not, all the data will loaded to the memory. 
     GPU friendly. Typically instantiated for train and test datasets. 
 
-    Parameter
+    Parameters
     ----------
     frame_list: list
         List containing the frame numbers. 
