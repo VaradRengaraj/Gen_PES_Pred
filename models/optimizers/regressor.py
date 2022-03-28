202c14c7f07656fd2e413573226ee42999f17ec1
Varadarajan <varadarajanrengaraj@outlook.com>
2022-03-28 18:55:54 +0200

    remove currently not supported optimizers

--- models/optimizers/regressor.py
+++ models/optimizers/regressor.py
@@ -1,31 +1,20 @@
-#!/usr/bin/env  python
-# encoding: utf-8
 
 class Regressor:
-    """ This class defines the local optimization method.
+    """ This class defines the optimizers
 
     Parameters
     ----------
-    method: str
-        Type of minimization scheme, e.g.: 'LBFGS'.
+    method: string
+        Name of the optimization method, e.g.: 'SGD'.
     user_kwargs: dict
-        The arguments for the optimization method. These arguments are 
-        passed with a dict.
+        The arguments typically coming from config file. 
     """
     def __init__(self, method, user_kwargs):
         self.method = method
         
         kwargs = {'lr': 1,}
-        if method in ['lbfgs', 'LBFGS']:
-            from pyxtal_ff.models.optimizers.lbfgs import LBFGS as optimizer
-            _kwargs = {'max_iter': 100,
-                       'max_eval': 15000,
-                       'tolerance_grad': 1e-7,
-                       'tolerance_change': 1e-9,
-                       'history_size': 10,
-                       'line_search_fn': 'strong_wolfe'}
         
-        elif self.method in ['SGD', 'sgd']:
+        if self.method in ['SGD', 'sgd']:
             from torch.optim import SGD as optimizer
             _kwargs = {'lr': 0.001,
                        'momentum': 0.,
@@ -41,14 +30,6 @@ class Regressor:
                        'weight_decay': 0, 
                        'amsgrad': False}
 
-        elif self.method in ['lbfgsb']:
-            from pyxtal_ff.models.optimizers.lbfgsb import LBFGSScipy as optimizer
-            _kwargs = {'max_iter': 100,
-                       'max_eval': 15000,
-                       'tolerance_grad': 1e-7,
-                       'tolerance_change': 1e-9,
-                       'history_size': 10}
-
         else:
             msg = f"The {method} is not implemented yet."
             raise NotImplementedError(msg)
@@ -63,7 +44,7 @@ class Regressor:
 
 
     def regress(self, models):
-        """ Define optimization scheme and return the optimizer to models.
+        """ Returns the optimizer to models.
 
         Parameters
         ----------
@@ -73,24 +54,14 @@ class Regressor:
         Returns
         -------
         regressor
-            PyTorch optimizer or Scipy LBFGS-B.
+            PyTorch optimizer.
         """
         try:
             params = models['model'].parameters()
         except:
             params = [p for model in models.values() for p in model.parameters()]
 
-        if self.method in ['LBFGS', 'lbfgs']:
-            regressor = self.optimizer(params, 
-                                       lr=self.kwargs['lr'],
-                                       max_iter=self.kwargs['max_iter'],
-                                       max_eval=self.kwargs['max_eval'],
-                                       tolerance_grad=self.kwargs['tolerance_grad'],
-                                       tolerance_change=self.kwargs['tolerance_change'],
-                                       history_size=self.kwargs['history_size'],
-                                       line_search_fn=self.kwargs['line_search_fn'])
-
-        elif self.method in ['SGD', 'sgd']:
+        if self.method in ['SGD', 'sgd']:
             regressor = self.optimizer(params,
                                        lr=self.kwargs['lr'],
                                        momentum=self.kwargs['momentum'],
@@ -106,12 +77,4 @@ class Regressor:
                                        weight_decay=self.kwargs['weight_decay'],
                                        amsgrad=self.kwargs['amsgrad'])
 
-        elif self.method in ['lbfgsb']: 
-            regressor = self.optimizer(params,
-                                       max_iter=self.kwargs['max_iter'],
-                                       max_eval=self.kwargs['max_eval'],
-                                       tolerance_grad=self.kwargs['tolerance_grad'],
-                                       tolerance_change=self.kwargs['tolerance_change'],
-                                       history_size=self.kwargs['history_size'])
-       
         return regressor
